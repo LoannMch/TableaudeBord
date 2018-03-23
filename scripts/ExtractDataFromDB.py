331e9ebcf1bc592ebf6daf1c4fe2065427748f50
Unknown <loann.michon@gmail.com>
2018-03-23 14:40:18 +0100

    Auto stash before merge of "master" and "origin/master"
    maj script extraction BD

--- scripts/ExtractDataFromDB.py
+++ scripts/ExtractDataFromDB.py
@@ -9,17 +9,6 @@ import requests
 import pyodbc
 import pandas as pd
 
-
-    
-sql = 'SELECT count(id_attack) as Nb_attack,lib_country \
-FROM attack, city ,country \
-WHERE attack.id_city = city.id_city \
-AND  city.id_country =country.id_country \
-GROUP BY lib_country \
-ORDER BY count(id_attack) DESC;'
-
-columns = ['nb_attack', 'country']
-
 def export_query_to_csv(sql, columns, name_csv):
     con = pyodbc.connect("Driver={SQL Server};"
                         "Server=LAPTOP-4H4QSG7A\SQLEXPRESS;"
@@ -29,9 +18,25 @@ def export_query_to_csv(sql, columns, name_csv):
     cursor.execute(sql)
     rows = cursor.fetchall()
     tab =[]
+    nb_col = len(columns)
     for row in rows:
-        tab.append([row[0], row[1]])  
+        col = []
+        for i in range(nb_col):
+            col.append(row[i])
+        tab.append(col)  
     df = pd.DataFrame(tab, columns=columns)
     df.to_csv('CSV_for_analyse/' + name_csv)
-  
-export_query_to_csv(sql, columns, 'pays_attaqué.csv')
+
+
+sql = 'SELECT count(id_attack) as Nb_attack,co1.lib_country as \'Attaqué\',co2.lib_country as \'Attaquant\' \
+FROM attack, attacker, city ci1, country co1, city ci2, country co2 \
+WHERE attack.id_city = ci1.id_city \
+AND  ci1.id_country =co1.id_country \
+AND attack.id_attacker = attacker.id_attacker \
+AND  attacker.id_city = ci2.id_city \
+AND  ci2.id_country =co2.id_country \
+GROUP BY co1.lib_country, co2.lib_country \
+ORDER BY count(id_attack) DESC;'
+
+columns = ['Nb_attack', 'Attaqué', 'Attaquant']  
+export_query_to_csv(sql, columns, 'type_attack.csv')
